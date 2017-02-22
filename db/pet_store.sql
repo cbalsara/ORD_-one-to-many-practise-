a46568b7fccbee43c5910d80a3222ff1faa19958
Cyrus <cdbalsara@icloud.com>
2017-02-22 18:27:41 +0000

    created ORD named pet_store and created stores & pets tables with pets foregin key referencing stores primary key

--- db/pet_store.sql
+++ db/pet_store.sql
@@ -1,14 +1,16 @@
-DROP TABLE 
-DROP TABLE 
+DROP TABLE pets;
+DROP TABLE stores;
 
--- CREATE TABLE pets(
--- id
--- name
--- type 
--- );
+CREATE TABLE stores(
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  address VARCHAR(255),
+  store_type VARCHAR(255)
+);
 
-
-
-
--- CREATE TABLE stores(
--- );
+CREATE TABLE pets(
+id SERIAL4,
+name VARCHAR(255),
+type VARCHAR(255),
+store_id SERIAL4 REFERENCES stores(id)
+);
